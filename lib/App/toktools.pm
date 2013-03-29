e7a4b29d740664a5dec30074a323e7c9d83704a2
tokuhirom <tokuhirom@gmail.com>
2013-03-29 13:56:54 +0900

    migrate.

---
+++ lib/App/toktools.pm
@@ -0,0 +1,34 @@
+package App::toktools;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+App::toktools - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::toktools;
+
+=head1 DESCRIPTION
+
+App::toktools is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
